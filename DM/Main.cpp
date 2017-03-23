193f177df74422c7cfddd4a8defb4a45999b07c8
fkti6307 <fkti6307>
2017-03-24 00:15:17 +0300

    Closing work with SUB_NN_N

--- DM/Main.cpp
+++ DM/Main.cpp
@@ -1,9 +1,25 @@
 #include "Header.h"
 
 
+void print(BigNatural);
+
 int main(void)
 {
-	printf("Hello world");
+	BigNatural first;
+	BigNatural second;
+
+	short firstCoef[] = { 1,1,1,1,1,1,1,1,1,1,1,1 };
+	short secondCoef[] = { 7,3 };
+
+	first.coef = firstCoef;
+	first.size = 12;
+
+	second.coef = secondCoef;
+	second.size = 2;
+
+	print(SUB_NN_N(first, second));
+
+
 	system("pause");
 	return 0;
 }
@@ -11,7 +27,7 @@ int main(void)
 
 void print(BigNatural number)
 {
-	for (int i = number.size - 1; i > 0; i--)
+	for (int i = number.size - 1; i >= 0; i--)
 	{
 
 
