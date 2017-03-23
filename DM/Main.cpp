ce9955062f73c2305823cb80308eb1c3c5845ef5
fkti6307 <fkti6307>
2017-03-23 23:43:17 +0300

    начал работу над меню

--- DM/Main.cpp
+++ DM/Main.cpp
@@ -1,20 +1,14 @@
 #include "Header.h"
 
 
+
+
+
 int main(void)
 {
+
 	printf("Hello world");
 	system("pause");
 	return 0;
 }
 
-
-void print(BigNatural number)
-{
-	for (int i = number.size - 1; i > 0; i--)
-	{
-
-
-		printf("%d", number.coef[i]);
-	}
-}
---
+++ DM/Menu.cpp
@@ -0,0 +1,159 @@
+#include "conio.h"
+#include "windows.h"
+#include "Header.h"
+
+
+
+enum Menu
+{
+	mainMenu,
+	criteriaMenu,
+	deleteMenu,
+	addMenu,
+	updateMenu,
+	outputMenu
+};
+
+
+//************************************************************************
+
+//Выбор меню
+int getMenu(Menu menu, char* message)
+{
+	static char *mainMenuPuncts[] = {
+		"Натуральные числа",
+		"Целые числа",
+		"Рациональные числа",
+		"Многочлены",
+		"Справка",
+		"Выход"
+	},
+		*naturalNumberMenuPuncts[] = {
+		"Сложить 2 числа",
+		"Вычесть из большего числа меньшее",
+		"Перемножить 2 числа",
+		"Частное от деления 2 чисел",
+		"Остаток от деления 2 чисел",
+		"НОД 2 чисел",
+		"НОК 2 чисел"
+	},
+		*integerNumberMenuPuncts[] = {
+		"Сложить 2 числа",
+		"Вычесть из большего числа меньшее",
+		"Перемножить 2 числа",
+		"Частное от деления 2 чисел",
+		"Остаток от деления 2 чисел",
+	},
+		*deleteMenuPuncts[] = {
+		"Удалить книгу по названию",
+		"Удалить все книги автора",
+		"Удалить все книги издательства",
+		"Назад"
+	},
+		*addMenuPuncts[] = {
+		"Добавить в начало",
+		"Добавить в конец",
+		"Назад"
+	},
+		*outputMenuPuncts[] = {
+		"Вывести в прямом порядке",
+		"Вывести в обратном порядке",
+		"Изменить кол-во книг на странице",
+		"Назад"
+	};
+
+	switch (menu)
+	{
+	case mainMenu:
+		return selectMenu(message, mainMenuPuncts, 6);
+	case criteriaMenu:
+		return selectMenu(message, criteriaMenuPuncts, 5);
+	case updateMenu:
+		return selectMenu(message, updateMenuPuncts, 4);
+	case deleteMenu:
+		return selectMenu(message, deleteMenuPuncts, 4);
+	case addMenu:
+		return selectMenu(message, addMenuPuncts, 3);
+	case outputMenu:
+		return selectMenu(message, outputMenuPuncts, 3);
+	}
+	return 0;
+}
+
+
+//************************************************************************
+
+//Выбор пункта меню
+int selectMenu(char* message, char** menu, int size)
+{
+	int current = 0;	//Текущий выбранный элемент
+	bool exit = false;	//Условие выхода
+
+	system("cls");
+	gotoXY(3, 1);
+	printf("%s", message);
+
+	do
+	{
+		printMenu(menu, size, current);
+		current = menuMove(current, size, &exit);
+	} while (!exit);
+
+	system("cls");
+	changeConsoleColor(0, 7);
+
+	return current;
+}
+
+//************************************************************************
+
+//Вывод меню на экран
+void printMenu(char** menu, int size, int current)
+{
+
+	for (int i = 0; i < size; i++)
+	{
+		gotoXY(0, i + 3);
+		changeConsoleColor(0, current == i ? 14 : 7);
+		printf("%-6s%s%s", current == i ? "  -->  " : "", menu[i], current == i ? "  <--" : "        ");
+	}
+}
+
+//************************************************************************
+
+//Движение по меню
+int menuMove(int current, int size, bool* exit)
+{
+	int code;
+	code = getch();
+	if (code == 224 || code == 0)
+		code = getch();
+	if (code == 72)
+		current = --current < 0 ? current + size : current;
+	else if (code == 80)
+		current = ++current % size;
+	*exit = code == 13;
+
+	return current;
+}
+
+//************************************************************************
+
+
+void gotoXY(short x, short y)
+{
+	static HANDLE outputHande = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	SetConsoleCursorPosition(outputHande, { x, y });
+}
+
+
+//************************************************************************
+
+
+void changeConsoleColor(int bg, int text)
+{
+	static HANDLE outputHande = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	SetConsoleTextAttribute(outputHande, bg << 4 | text);
+}
