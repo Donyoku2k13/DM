f9df7d2caa879c87445fd161e19396b1e7128afb
Кичик <@DESKTOP-48FN812>
2017-03-24 14:22:49 +0300

    Если надо будет прям по красоте, то можно ввести лишнюю переменную)

--- DM/RationalFraction.cpp
+++ DM/RationalFraction.cpp
@@ -1,3 +1,15 @@
 #include "Header.h"
 
 //В этом файле должны быть ТОЛЬКО реализации методов для работы с рациональными числами
+
+RationalFraction TRANS_Z_Q(BigInteger number)
+{
+	RationalFraction rezult;
+
+	(short*)malloc(sizeof(short) * (1));
+	rezult.numenator = number;
+	rezult.denominator.coef = { (short*)malloc(sizeof(short) * (1)) };
+	rezult.denominator.size = 1;
+	return rezult;
+}
+
