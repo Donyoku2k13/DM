aa77f28bf46c491898af84be57117761882ec1f8
Grizzly <Grizzly@GRIZZLY-MAIN>
2017-03-24 17:41:23 +0300

    DIV_NN_N Commented

--- DM/BigNatural.cpp
+++ DM/BigNatural.cpp
@@ -67,27 +67,27 @@ BigNatural DIV_NN_N(BigNatural first, BigNatural second)
   if (COM_NN_D(first, second) == 1)
     return DIV_NN_N(second, first);
 
-  BigNatural first_c;
-  BigNatural k = BigNatural();
-  if (!NZER_N_B)
+  BigNatural first_c; //Копия делимого
+  BigNatural k = BigNatural(); //Счетчик деления
+  if (!NZER_N_B) //Если делитель = нулю
     printf("Делитель должен быть больше нуля!");
   else
   {
-    if(COM_NN_D(first, second)==2)
+    if(COM_NN_D(first, second)==2) //Если первое больше второго
     {
-      first_c = first;
+      first_c = first; //Копируем Делимое
       do
       {
-        first_c = SUB_NN_N(first, second);
-        k = ADD_1N_N(k);
-      } while (COM_NN_D(first_c, second) == 2 || COM_NN_D(first_c, second) == 0);
-        return k;
+        first_c = SUB_NN_N(first, second); //Вычитаем из делимого делитель
+        k = ADD_1N_N(k); //Увеличиваем счетчик деления на 1
+      } while (COM_NN_D(first_c, second) == 2 || COM_NN_D(first_c, second) == 0); //Пока делимое больше делителя
+        return k; //Возвразаем счетчик (частное от целочисленного деления)
     }
     else
-    if (COM_NN_D(first, second) == 0)
+    if (COM_NN_D(first, second) == 0) //Если первое равно второму
     {
-      k = ADD_1N_N(k);
-      return k;
+      k = ADD_1N_N(k); //Увеличиваем счетчик до 1
+      return k; //Возвращаем единицу
     }
 
 }
