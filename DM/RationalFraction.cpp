8fecd1f9194195c3969eb4bd81fdb9f7982fb13d
Кичик <@DESKTOP-48FN812>
2017-03-24 15:50:21 +0300

    .

--- DM/RationalFraction.cpp
+++ DM/RationalFraction.cpp
@@ -1,3 +1,12 @@
 #include "Header.h"
 
 //В этом файле должны быть ТОЛЬКО реализации методов для работы с рациональными числами
+BigInteger TRANS_Q_Z(RationalFraction number);
+{
+	BigInteger result;
+	if (result.denominator == 1)
+		result.*numenator = number.numenator;
+	else
+		printf("Число не может быть представлено в виде целого");
+	return result;
+}
