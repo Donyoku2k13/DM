a591c06b34c2c9976f255f94492c41514fd74b39
fkti6307 <fkti6307>
2017-03-24 14:52:44 +0300

    исправил ошибки, готово к слиянию

--- DM/RationalFraction.cpp
+++ DM/RationalFraction.cpp
@@ -6,9 +6,9 @@ RationalFraction TRANS_Z_Q(BigInteger number)
 {
 	RationalFraction rezult;
 
-	(short*)malloc(sizeof(short) * (1));
 	rezult.numenator = number;
-	rezult.denominator.coef = { (short*)malloc(sizeof(short) * (1)) };
+	rezult.denominator.coef =  (short*)malloc(sizeof(short) * (1));
+	rezult.denominator.coef[0] = 1;
 	rezult.denominator.size = 1;
 	return rezult;
 }
