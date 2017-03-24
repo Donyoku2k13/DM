6615493a57f2cda6565eb0aa90cacd074b3babff
fkti6307 <fkti6307>
2017-03-25 00:22:58 +0300

    добавил производную

--- DM/Polynom.cpp
+++ DM/Polynom.cpp
@@ -7,3 +7,21 @@ int DEG_P_N(Polynom polynom)
 {
 	return polynom.degree;
 }
+
+
+Polynom DER_P_P(Polynom polynom)
+{
+	Polynom result;
+
+
+	result.degree = polynom.degree - 1;
+	result.coef = (RationalFraction*)malloc(polynom.degree * sizeof(RationalFraction));
+
+	for (int i = 0; i < polynom.degree; i++)
+	{
+		result.coef[i].numenator.number = MUL_ND_N(polynom.coef[i].numenator.number, polynom.degree - i);
+		result.coef[i].denominator = polynom.coef[i].denominator;
+	}
+
+	return result;
+}
