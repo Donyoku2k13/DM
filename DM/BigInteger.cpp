d4a0ce40481afcff589616d550d4c4a61b48aca4
Кичик <@DESKTOP-48FN812>
2017-03-24 21:07:50 +0300

    За Любу

--- DM/BigInteger.cpp
+++ DM/BigInteger.cpp
@@ -1,3 +1,13 @@
 #include "Header.h"
 
 //В этом файле должны быть ТОЛЬКО реализации методов для работы с целыми числами
+BigInteger MUL_ZZ_Z(BigInteger first, BigInteger second)
+{
+	BigInteger result;
+	result.number = MUL_NN_N(ABS_Z_N(first), ABS_Z_N(second));
+	if (first.sign == second.sign)
+		result.sign = plus;
+	else
+		result.sign = minus;
+	return result;
+}
