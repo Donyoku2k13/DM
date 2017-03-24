ab02ff602482257dc0027d8c617e51117823cc43
Кичик <@DESKTOP-48FN812>
2017-03-24 15:40:51 +0300

    /

--- DM/RationalFraction.cpp
+++ DM/RationalFraction.cpp
@@ -1,3 +1,18 @@
 #include "Header.h"
 
 //В этом файле должны быть ТОЛЬКО реализации методов для работы с рациональными числами
+
+RationalFraction ADD_QQ_Q(RationalFraction first, RationalFraction second);
+{
+	RationalFraction result; // 
+	BigInteger nokfirst, noksecond; // НОК, деленный на знаменатель(Тип верный?)
+
+	nokfirst = LCM_NN_N(first.denuminator, second.denuminator) / (first.denominator);
+	noksecond = LCM_NN_N(first.denuminator, second.denuminator) / (second.denominator);
+	result.numenator = ADD_ZZ_Z(MUL_ZZ_Z((nokfirst, first.numinator), MUL_ZZ_Z(noksecond, second.numenator)); // расчитываем числитель
+	result.denominator = LCM_NN_N(first.denuminator, second.denuminator);                                 // расчитываем общий знаменатель 
+	return result;
+}
+
+
+
