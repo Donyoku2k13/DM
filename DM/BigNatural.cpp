3c4f28fa1322a5eed48884caaba8d453fe25e94b
NickSer <nicks.serge@gmail.com>
2017-03-24 19:00:27 +0300

    NZER_N_B  ADD_1N_N

--- DM/BigNatural.cpp
+++ DM/BigNatural.cpp
@@ -62,4 +62,53 @@ BigNatural SUB_NN_N(BigNatural first, BigNatural second)
 }
 
 
+//Проверка на ноль
+bool NZER_N_B(BigNatural number)
+{
+	int i;
+	for (i = 0; i < number.size; i++)
+	{
+		if (number.coef[i] != 0) // Если хотя бы одна цифра не равна нулю, то возвращаем true
+		{
+			return true;
+		}
+	}
+	return false; 
+}
+
+//Добавление единицы к длинному числу
+BigNatural ADD_1N_N(BigNatural number)
+{
+	int counter = 1;
+	BigNatural result;
+	result = number;
+	if (result.coef[result.size - 1] != 9) // Если последняя цифра не равна девяти, прибавляем единицу
+	{
+		result.coef[result.size - 1]++;
+	}
+	else
+	{
+		while (result.coef[result.size - counter] == 9 && counter != result.size) // Если последняя цифра не равна 9, то ищем первую цифру не равную 9, прибавляем к ней единицу, и всем предыдущем присваиваем 0
+		{
+			result.coef[result.size - counter] = 0;
+			counter++;
+		}
+		if (counter == result.size) // Если все цифры равны 9, увеличиваем массив на единицу, первой цифре присваеваем 1, всем остальным присваеваем 0
+		{
+			result.coef = (short*) realloc(result.coef, sizeof(short)* (result.size + 1));
+			result.coef[0] = 1;
+			for (int i = 1; i < result.size; i++)
+			{
+				result.coef[i] = 0;
+			}
+		}
+		else
+		{
+			result.coef[result.size - counter]++;
+		}
+	}
+	return result;
+}
+
+
 
