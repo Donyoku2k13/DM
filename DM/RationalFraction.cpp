eae5bc3bae07db2603cc591013a4cdfe360cc984
fkti6307 <fkti6307>
2017-03-24 17:50:13 +0300

    убрал свои комменты

--- DM/RationalFraction.cpp
+++ DM/RationalFraction.cpp
@@ -4,13 +4,13 @@
 BigInteger TRANS_Q_Z(RationalFraction number)
 {
 	BigInteger result;
-	//Результат - целое, у него нет знаменателя
+
 	if (number.denominator.size == 1 && number.denominator.coef[0]==1)
-	{//Эта звездочка явно не отсюда, и опять же в результате нет числителя
+	{
 		result = number.numenator;
 		result.sign = number.numenator.sign;
 	}
-	//Не мешало бы еще и знак числа сохранять в результате
+
 	else
 		printf("Число не может быть представлено в виде целого");
 	return result;
