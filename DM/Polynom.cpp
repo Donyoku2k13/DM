afd847400d65103f96386ab495eaf344b24faf07
FroseMan <iazarev@yahoo.com>
2017-03-24 23:12:01 +0300

    Жмойда производная

--- DM/Polynom.cpp
+++ DM/Polynom.cpp
@@ -7,3 +7,17 @@ int DEG_P_N(Polynom polynom)
 {
 	return polynom.degree;
 }
+
+
+Polynom DER_P_P(Polynom polynom)
+{
+	Polynom first_numenator;
+	first_numenator.coef = (RationalFraction*)malloc(polynom.degree * sizeof(RationalFraction));
+	first_numenator.coef->numenator.number.coef = (short*)malloc(polynom.coef->numenator.number.size * sizeof(short));
+	for (int i = 0; i<polynom.coef->denominator.size; i++)
+	{
+		first_numenator.coef[i].numenator.number.coef[i] = polynom.coef[i].numenator.number.coef[i] * polynom.degree;
+		polynom.degree--;
+	}
+	return first_numenator;
+}
