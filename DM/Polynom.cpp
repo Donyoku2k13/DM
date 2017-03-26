3cf0c7e0c4310078c1d8f2a17568e3d0deae8caf
Пользователь <@LAPTOP-01QLKR62>
2017-03-26 16:29:10 +0300

    Add function NOD polynom

--- DM/Polynom.cpp
+++ DM/Polynom.cpp
@@ -1,3 +1,17 @@
 #include "Header.h"
 
 //В этом файле должны быть ТОЛЬКО реализации методов для работы с многочленами
+
+Polynom GCF_PP_P(Polynom first, Polynom second)
+{
+	Polynom ost;
+
+	while (DEG_P_N(second) != 0 || NZER_N_B(TRANS_Z_N(second.coef[0].numenator)))
+	{
+		ost = MOD_PP_P(first, second);
+		first = second;
+		second = ost;
+	}
+
+	return first;
+}
